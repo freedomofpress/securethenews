905dfdfe0194ad1d0cd39780282bfb2b46f483f3
Garrett Robinson <garrett.f.robinson@gmail.com>
2016-08-02 16:58:45 -0700

    Basic Site model
    * A site had no name... it does now
    * URLField seemed like a good type of field for a site's URL, but in the
      Admin Interface the validator had the annoying property of trying to
      automatically prepend a scheme to the URL in order to make it valid.
      Since the whole point of this project is to figure out whether and how a
      site supports HTTP vs HTTPS, this behavior was annoying and I
      anticipated it leading to confusion later, so I changed this field to
      a URLField.

--- securethenews/scans/models.py
+++ securethenews/scans/models.py
@@ -2,8 +2,17 @@ from django.db import models
 
 
 class Site(models.Model):
-    url = models.URLField(unique=True)
+    name = models.CharField('Name', max_length=255, unique=True)
+    url = models.CharField(
+        'URL',
+        max_length=255,
+        unique=True,
+        help_text='Specify the domain name without the scheme, e.g. "example.com" instead of "https://example.com"')
+
     added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
     
 
 class Scan(models.Model):
