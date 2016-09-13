8aade7710a0668dea9d62080029c0dcedeabbd3e
Garrett Robinson <garrett.f.robinson@gmail.com>
2016-09-12 23:02:08 -0700

    Add slug to Site models
    Using a slug for each site allows us to generate pretty URLs for each
    site's status page. The slugs are automatically derived from the site's
    unique name.
    
    The migrations for this were non-trivial and required custom code
    because they added unique fields to existing models. I used the 3-step
    approach outlined in the official Django docs:
    
    https://docs.djangoproject.com/en/1.10/howto/writing-migrations/#migrations-that-add-unique-fields.

--- securethenews/scans/models.py
+++ securethenews/scans/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class Site(models.Model):
     name = models.CharField('Name', max_length=255, unique=True)
+    slug = models.SlugField('Slug', unique=True, editable=False)
     url = models.CharField(
         'URL',
         max_length=255,
@@ -13,7 +15,10 @@ class Site(models.Model):
 
     def __str__(self):
         return self.name
-    
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Site, self).save(*args, **kwargs)
 
 class Scan(models.Model):
     site = models.ForeignKey(
