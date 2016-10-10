d01adfce2b63c37341944ff8138bc99d09905726
Gabe Isman <gabe.isman@gmail.com>
2016-10-10 12:35:30 -0400

    Add custom template tag for grades.

---
+++ securethenews/sites/templatetags/site_tags.py
@@ -0,0 +1,24 @@
+from django import template
+
+register = template.Library()
+
+@register.inclusion_tag('sites/grade.html')
+def grade(site):
+    scan = site.scans.latest('timestamp')
+    score = scan.score
+    if score > 80:
+        grade = 'A'
+        class_name = 'grade-a'
+    elif score > 60:
+        grade = 'B'
+        class_name = 'grade-b'
+    elif score > 40:
+        grade = 'C'
+        class_name = 'grade-c'
+    elif score > 20:
+        grade = 'D'
+        class_name = 'grade-d'
+    else:
+        grade = 'F'
+        class_name = 'grade-f'
+    return { 'grade': grade, 'class_name': class_name }
