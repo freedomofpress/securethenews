c2bc102d1e039ca396382536594f190b0f97b54d
Garrett Robinson <garrett@freedom.press>
2016-11-04 07:18:12 -0700

    Improve wagtailmodeladmin for Sites
    - Use a more appropriate icon
    - Display useful info in columns on IndexView via list_display

--- securethenews/sites/wagtail_hooks.py
+++ securethenews/sites/wagtail_hooks.py
@@ -4,8 +4,15 @@ from .models import Site
 class SiteAdmin(ModelAdmin):
     model = Site
     menu_label = 'News Sites'
-    menu_icon = 'date'
+    menu_icon = 'site'
     add_to_settings_menu = False
+
+    list_display = ('name', 'domain', 'score')
+
+    def score(self, obj):
+        return '{} / 100'.format(obj.scans.latest().score)
+    score.short_description = 'Score'
+
     search_fields = ('name', 'domain')
 
 modeladmin_register(SiteAdmin)
