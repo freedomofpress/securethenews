6d42905fb34c131e7c4dcf4403d2a829ad03f756
Garrett Robinson <garrett@freedom.press>
2016-11-04 07:19:46 -0700

    Basic moderation for Pledges via wagtailmodeladmin
    Adds an entry in the sidebar to view and edit Pledges through the
    Wagtail admin. This enables a basic approval workflow:
    
    1. Check Pledges in Wagtail admin
    2. Review Pledges that have not yet been approved
    3. If a pledge looks good, approve it and save the change to the model.
    4. Otherwise, delete it.
    
    Ideally, the only field of the Pledge model that would be editable in
    the admin would be the "Approved" BooleanField. Making the other fields
    read-only would remove the potential for an admin to accidentally modify
    a pledge. Unfortunately, this is not currently possible in
    wagtailmodeladmin without significant customization. There is an open
    issue: https://github.com/torchbox/wagtail/issues/2893.

--- securethenews/sites/wagtail_hooks.py
+++ securethenews/sites/wagtail_hooks.py
@@ -1,5 +1,6 @@
 from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
-from .models import Site
+
+from .models import Site, Pledge
 
 class SiteAdmin(ModelAdmin):
     model = Site
@@ -15,4 +16,17 @@ class SiteAdmin(ModelAdmin):
 
     search_fields = ('name', 'domain')
 
+class PledgeAdmin(ModelAdmin):
+    model = Pledge
+    menu_label = 'Pledges'
+    menu_icon = 'form'
+    add_to_settings_menu = False
+
+    list_display = ('site', 'timestamp', 'approved')
+    list_filter = ('approved',)
+    ordering = ('approved',)
+
+    search_fields = ('url', 'contact_email')
+
 modeladmin_register(SiteAdmin)
+modeladmin_register(PledgeAdmin)
