143169af166a1380c8e7ec532aca4f1ebb16aa49
Garrett Robinson <garrett@freedom.press>
2016-11-15 12:35:59 -0800

    Don't append a trailing slash to Wagtail URLs
    The trailing slash looks ugly, particularly in combination with the
    anchor used in the header permalinks.
    
    http://docs.wagtail.io/en/v1.7/advanced_topics/settings.html#append-slash

--- securethenews/securethenews/settings/base.py
+++ securethenews/securethenews/settings/base.py
@@ -149,3 +149,5 @@ WAGTAIL_SITE_NAME = "securethenews"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://example.com'
+
+WAGTAIL_APPEND_SLASH = False
