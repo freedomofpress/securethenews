be1b1e8d4f3b91e182e78624d02bc1d56d5aafa6
Garrett Robinson <garrett@freedom.press>
2016-11-18 05:06:00 -0800

    gulp tasks for building production assets

--- securethenews/gulpfile.js
+++ securethenews/gulpfile.js
@@ -5,63 +5,79 @@ var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var browserify = require('browserify');
 var watchify = require('watchify');
-var babel = require('babelify');
+var babelify = require('babelify');
 var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
 var autoprefixer = require('gulp-autoprefixer');
 var livereload = require('gulp-livereload');
 var jadeify = require('jadeify');
 
+const buildDir = './client/build';
 
-function compile(watch) {
-  var bundler = watchify(browserify('./client/src/javascript/index.js', { debug: true })
-    .transform(jadeify)
-    .transform(babel),
-    { poll: true });
+gulp.task('js', function() {
+  var bundler = browserify('./client/src/javascript/index.js', {
+      debug: true
+    }).transform(jadeify)
+      .transform(babelify);
 
-  function rebundle() {
-    bundler.bundle()
-      .on('error', function(err) { console.error(err); this.emit('end'); })
+  bundler = watchify(bundler, {
+    poll: true
+  });
+
+  var rebundle = function() {
+    return bundler.bundle()
+      .on('error', function(err) {
+        console.error(err);
+        this.emit('end');
+      })
       .pipe(source('build.js'))
       .pipe(buffer())
       .pipe(sourcemaps.init({ loadMaps: true }))
       .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest('./client/build'))
+      .pipe(gulp.dest(buildDir))
       .pipe(livereload())
       .on('end', function() {
         gulpUtil.log('build.js rebuilt');
       });
   }
 
-  if (watch) {
-    bundler.on('update', rebundle);
-  }
+  bundler.on('update', rebundle);
+  return rebundle();
+});
 
-  rebundle();
-}
+gulp.task('js:production', function() {
+  return browserify('./client/src/javascript/index.js', {
+    debug: false
+  }).transform(jadeify)
+    .transform('babelify', {presets: ["es2015"]})
+    .bundle()
+    .pipe(source('build.js'))
+    .pipe(gulp.dest(buildDir))
+    .on('error', function() {
+      gulpUtil.log('JS error: <%= error.message %>')
+    })
+    .on('end', function() {
+      gulpUtil.log('production js built');
+    })
+});
 
-// Compile Sass to css
 gulp.task('styles', function() {
-  gulp.src('client/src/styles/main.scss')
+  return gulp.src('client/src/styles/main.scss')
     .pipe(plumber())
     .pipe(sass({
       includePaths: ['node_modules/']
     }).on('error', sass.logError))
     .pipe(autoprefixer())
-    .pipe(gulp.dest('./client/build'))
+    .pipe(gulp.dest(buildDir))
     .pipe(livereload())
     .on('error', gulpUtil.log);
 });
 
-// TODO: Lint all the javascript task
+gulp.task('build:production', ['styles', 'js:production']);
 
-function watch() {
+gulp.task('watch', ['styles', 'js'], function() {
   livereload.listen();
-  gulp.watch('client/src/**/*.scss', ['styles']);
-  return compile(true);
-};
+  gulp.watch('./client/src/**/*.scss', ['styles']);
+});
 
-gulp.task('build', function() { return compile(); });
-gulp.task('watch', function() { return watch(); });
-
-gulp.task('default', ['watch', 'styles']);;
+gulp.task('default', ['watch']);
