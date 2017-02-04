d3a360132f8fc634bceb1bf1f8eeba2ace271fb9
Garrett Robinson <garrett@freedom.press>
2017-02-03 17:10:53 -0800

    Frontend cache invalidation for BlogIndexPage when BlogPosts published/unpublished

--- securethenews/blog/apps.py
+++ securethenews/blog/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class BlogConfig(AppConfig):
     name = 'blog'
+
+    def ready(self):
+        import blog.signals
---
+++ securethenews/blog/signals.py
@@ -0,0 +1,23 @@
+from wagtail.wagtailcore.signals import page_published, page_unpublished
+from wagtail.contrib.wagtailfrontendcache.utils import purge_page_from_cache
+
+from blog.models import BlogIndexPage, BlogPost
+
+
+def invalidate_frontend_cache_for_blog_index_page(sender, instance, **kwargs):
+    """Invalidate the frontend cache for the parent BlogIndexPage of a BlogPost."""
+    blog_post = instance
+    # Recommended way to get parent page from https://github.com/wagtail/wagtail/issues/2779#issuecomment-228472829
+    blog_index_page = blog_post.get_parent()
+    if blog_index_page:
+        purge_page_from_cache(blog_index_page)
+
+
+page_published.connect(
+    invalidate_frontend_cache_for_blog_index_page,
+    sender=BlogPost
+)
+page_unpublished.connect(
+    invalidate_frontend_cache_for_blog_index_page,
+    sender=BlogPost
+)
