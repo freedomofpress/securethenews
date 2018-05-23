8e8f1493f469213442469b24b19eefa7ec2e1949
Conor Schaefer <conor@freedom.press>
2018-05-22 18:31:58 -0700

    Refactors container entrypoint script
    Ported ash -> bash, for better error handling. Removed dependency on the-
    "wait-for-it" script, opting instead to use `nc -z` directly. The sole-
    loss of functionality here is that the `nc -z` will retry indefinitely.
    We can add logic to timeout if that becomes a problem.
    
    Also inverts the conditional logic governing prod/dev functionality.
    Now, the script defaults to prod functionality (e.g. prefer gunicorn
    over runserver), running the dev-only logic if-and-only-if
    DEPLOY_ENV=dev. Hopefully this prevents misconfigurations in prod.

--- docker/django-start.sh
+++ docker/django-start.sh
@@ -1,10 +1,12 @@
-#!/bin/ash
-#
+#!/bin/bash
+# Container entrypoint script for Django applications.
+set -e
+
 
 wait_for_node() {
-    if [ "${DEPLOY_ENV}" != "prod" ]; then
-        echo Waiting for node to start..
-        while [ ! -f .node_complete ]
+    if [ "${DEPLOY_ENV}" == "dev" ]; then
+        echo "Waiting for node to start..."
+        until -f .node_complete
         do
             sleep 2
         done
@@ -13,20 +15,22 @@ wait_for_node() {
 }
 
 wait_for_postgres() {
-    echo Waiting for postgres to start..
-    wait-for-it.sh -t 120 -h ${DJANGO_DB_HOST} -p ${DJANGO_DB_PORT}
+    echo "Waiting for postgres to start..."
+    until nc -z "${DJANGO_DB_HOST}" -p "${DJANGO_DB_PORT}"
+    do
+        sleep 2
+    done
 }
 
 django_start() {
-    if [ "${DEPLOY_ENV}" == "prod" ]; then
-        ./manage.py migrate
+    ./manage.py migrate
+    if [ "${DEPLOY_ENV}" == "dev" ]; then
+        ./manage.py runserver 0.0.0.0:8000
+    else
         ./manage.py collectstatic -c --noinput
         gunicorn -c /etc/gunicorn/gunicorn.py securethenews.wsgi
-    else
-        ./manage.py migrate && ./manage.py runserver 0.0.0.0:8000
     fi
 }
-
 
 wait_for_postgres
 wait_for_node
