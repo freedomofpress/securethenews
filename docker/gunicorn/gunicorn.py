9136b0fbe695c662fa2d321bfb4174fd3bf4942d
Michael Sheinberg <mike@freedom.press>
2018-06-20 16:39:34 -0400

    Add ability to over-ride thread/workers in prod

--- docker/gunicorn/gunicorn.py
+++ docker/gunicorn/gunicorn.py
@@ -5,3 +5,8 @@ group = os.environ.get('DJANGO_GCORN_GROUP', 'gcorn')
 bind = os.environ.get('DJANGO_GCORN_BIND', '0.0.0.0:8000')
 loglevel = os.environ.get('DJANGO_GCORN_LOGLEVEL', 'debug')
 capture_output = os.environ.get('DJANGO_GCORN_CAPOUTPUT', False)
+# See http://docs.gunicorn.org/en/stable/design.html for deployment
+# considerations. Using threads assumes use of the gthread worker which
+# is not the default.
+workers = int(os.environ.get('DJANGO_GCORN_WORKERS', 6))
+threads = int(os.environ.get('DJANGO_GCORN_THREADS', 1))
