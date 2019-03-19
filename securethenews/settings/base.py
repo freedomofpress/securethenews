57a8ac685906aab316b34ab6ed723c57b395b7d4
Cameron Higby-Naquin <me@chigby.org>
2019-03-19 10:59:08 -0400

    Remove `SessionAuthenticationMiddleware`
    Under Django 2.0, the session auth middleware is always enabled, and
    also "old-style" middleware declarations are not supported.
    
    See: https://docs.djangoproject.com/en/2.1/releases/2.0/#miscellaneous

--- securethenews/settings/base.py
+++ securethenews/settings/base.py
@@ -69,13 +69,12 @@ INSTALLED_APPS = [
     'django_logging'
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
