507ef383ed046d5857f4de9fa007f772297f1b84
Cameron Higby-Naquin <me@chigby.org>
2019-05-29 13:56:16 -0400

    deploy info: Make trailing slash optional in URL
    This is a workaround for what we think is a bug in wagtail, see:
    
    wagtail/wagtail#5331
    
    for more information.

--- home/wagtail_hooks.py
+++ home/wagtail_hooks.py
@@ -7,5 +7,5 @@ from .views import deploy_info_view
 @hooks.register('register_admin_urls')
 def urlconf_time():
     return [
-        url(r'^version/$', deploy_info_view, name='deployinfo'),
+        url(r'^version/?$', deploy_info_view, name='deployinfo'),
     ]
