86767bdb4efc0aafc6b1cb5f97f98e5ccfc4c79a
Charles Boyd <cboyd@kpd-i.com>
2019-06-28 16:45:06 -0400

    still troubleshooting building prod containers issue

--- scripts/ci-runner.sh
+++ scripts/ci-runner.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+set -x
+
 exit_code=0
 
 # Run django tests, we want to capture this exit code and fail but
 # not stop the script right away
 make app-tests-prod || exit_code=1
 
-# Copy XML from container into local environment
-docker cp $(docker-compose ps -q django):/django-logs/app-tests.xml ./test-results/
+# get django container ID
+DJANGO_CONTAINER_ID=$(docker-compose ps -q django)
 
-# Run testinfra tests
-make ops-tests || exit_code=1
+if [ -z "${DJANGO_CONTAINER_ID}" ]; then
+    echo "Django container failed to start, bailing out!"
+    exit_code=-1
+else
+    # Copy XML from container into local environment
+    docker cp ${DJANGO_CONTAINER_ID}:/django-logs/app-tests.xml ./test-results/
+
+    # Run testinfra tests
+    make ops-tests || exit_code=1
+fi
 
 exit "$exit_code"
