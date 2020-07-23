9588e8be58ba87b3b0143a32773d5b73fe70900c
Maeve Andrews <maeve@freedom.press>
2020-07-23 12:17:14 -0400

    Fix typo in version-file.sh

--- scripts/version-file.sh
+++ scripts/version-file.sh
@@ -13,7 +13,7 @@ cat /dev/null > "${DEPLOY_FILE}"
 echo -e "## GIT INFO #####\n" >> "${DEPLOY_FILE}"
 
 git_branch="$(git symbolic-ref --short HEAD 2>/dev/null || echo "N/A")"
-echo -e "git branch: ${git_branch_info}\n" >> "${DEPLOY_FILE}"
+echo -e "git branch: ${git_branch}\n" >> "${DEPLOY_FILE}"
 
 case "$git_branch" in
     N/A)
@@ -24,6 +24,7 @@ case "$git_branch" in
         echo -e "last release and commits since: $(git describe)" >> "${DEPLOY_FILE}";;
 esac
 
+echo >> "${DEPLOY_FILE}"
 git log -5 --oneline >> "${DEPLOY_FILE}"
 
 echo -e "\n## PYTHON INFO #####" >> "${DEPLOY_FILE}"
