207d0b109d3223ebc53c271ae2122d5b9da6de45
redshiftzero <jen@freedom.press>
2020-09-09 17:15:54 -0400

    onion-location: check meta http-equiv attribute

--- sites/management/commands/scan.py
+++ sites/management/commands/scan.py
@@ -1,4 +1,7 @@
 import json
+import logging
+from lxml import etree, html
+import requests
 import subprocess
 
 from django.core.management.base import BaseCommand, CommandError
@@ -6,6 +9,9 @@ from django.db import transaction
 
 from sites.models import Site, Scan
 
+logger = logging.getLogger(__name__)
+
+TIMEOUT_REQUESTS = 5
 
 def pshtt(domain):
     pshtt_cmd = ['pshtt', '--json', '--timeout', '5', domain]
@@ -39,6 +45,25 @@ def is_onion_available(pshtt_results):
                 onion_available = True
         except KeyError:
             pass
+
+    # If the header is not provided, it's possible the news organization
+    # has included it the HTML of the page in a meta tag using the `http-equiv`
+    # attribute.
+    if not onion_available:
+        for key in ["https", "httpswww"]:
+            try:
+                r = requests.get(pshtt_results["endpoints"][key]["url"],
+                                 timeout=TIMEOUT_REQUESTS)
+
+                tree = html.fromstring(r.content)
+                matching_meta_tags = tree.xpath('//meta[@http-equiv="onion-location"]/@content')
+                if len(matching_meta_tags) >= 1:
+                    onion_available = True
+            except KeyError:
+                pass
+            except (etree.ParserError, requests.exceptions.RequestException) as e:
+                # Error when requesting or parsing the page content, we log and continue on.
+                logger.error(e)
 
     return onion_available
 
